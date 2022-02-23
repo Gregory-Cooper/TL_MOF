e743131681e92b588c9be4c776bed3c070856dfe
Gregory-Cooper <80483587+Gregory-Cooper@users.noreply.github.com>
2022-02-23 17:20:10 -0500

    Add files via upload

--- transfer_learning.py
+++ transfer_learning.py
@@ -26,3 +26,32 @@ class NeuralNet(nn.Module):
 		out = self.relu(out)
 		out = self.fc3(out)
 		return out 
+
+class NeuralNet_sherpa_optimize(nn.Module):
+	def __init__(self, input_size,output_size, Parameters):
+		super(NeuralNet_sherpa_optimize, self).__init__()
+		#unpack sherpa package note 2 properties used outside
+		hidden_size_1=Parameters["H_l1"]
+		hidden_size_2=Parameters["H_l2"]
+		activate=Parameters["activate"]
+		#general dictionary for activation functions
+		self.dic_activation={
+			"nn.Hardswish" : nn.Hardswish(),
+			"nn.PReLU" : nn.PReLU(),
+			"nn.ReLU" : nn.ReLU(),
+			"nn.Sigmoid" : nn.Sigmoid(),
+			"nn.LeakyReLU" :nn.LeakyReLU(),
+		} 
+		self.fc1 = nn.Linear(input_size, hidden_size_1)
+		self.fc2 = nn.Linear(hidden_size_1, hidden_size_2)
+		self.fc3 = nn.Linear(hidden_size_2, output_size)
+		# set activation
+		self.activate = self.dic_activation[activate]
+
+	def forward(self, x):
+		out = self.fc1(x)
+		out = self.activate(out)
+		out = self.fc2(out)
+		out = self.activate(out)
+		out = self.fc3(out)
+		return out 
