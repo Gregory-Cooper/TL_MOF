fa75af5bfc3f8a2b4ba7a7b145a5ce7d1862771c
Gregory-Cooper <80483587+Gregory-Cooper@users.noreply.github.com>
2022-05-19 14:10:00 -0400

    stuff

--- Functions/__init__.py
+++ Functions/__init__.py
@@ -1,4 +1,4 @@
 from .engine import get_processed_data,prep_data_splits
-from .Statistics_helper import make_pca_agg_fit,make_pca_gausian_fit,sample_cluster_frame,add_pca_and_graph,plot_outline,plot_dendrogram,size_clusters
+from .Statistics_helper import make_pca_agg_fit,make_pca_gausian_fit,sample_cluster_frame,add_pca_and_graph,plot_outline,plot_dendrogram,size_clusters,stratified_cluster_sample
 from .helper import anaylsis
 from .dataset_loader import data_loader,all_filter,get_descriptors,one_filter,data_scaler
