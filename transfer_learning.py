2af85a8e89f971e6b62833aa44774e9bf7f85873
Greg Cooper <coopergregorym@gmail.com>
2022-05-27 15:43:46 -0400

    std update

--- transfer_learning.py
+++ transfer_learning.py
@@ -72,3 +72,32 @@ class MyDataset(Dataset):
 
 	def __getitem__(self,idx):
 		return self.x_train[idx],self.y_train[idx]
+
+class NeuralNet_sherpa_optimize_1(nn.Module):
+	def __init__(self, input_size,output_size, Parameters):
+		super(NeuralNet_sherpa_optimize_1, self).__init__()
+		#unpack sherpa package note 2 properties used outside
+		hidden_size_1=Parameters["H_l1"]
+		hidden_size_2=int(Parameters["H_l1"]/2) #found by first trial set anaylsis
+		activate=Parameters["activate"]
+		#general dictionary for activation functions
+		self.dic_activation={
+			"nn.Hardswish" : nn.Hardswish(),
+			"nn.PReLU" : nn.PReLU(),
+			"nn.ReLU" : nn.ReLU(),
+			"nn.Sigmoid" : nn.Sigmoid(),
+			"nn.LeakyReLU" :nn.LeakyReLU(),
+		} 
+		self.fc1 = nn.Linear(input_size, hidden_size_1)
+		self.fc2 = nn.Linear(hidden_size_1, hidden_size_2)
+		self.fc3 = nn.Linear(hidden_size_2, output_size)
+		# set activation
+		self.activate = self.dic_activation[activate]
+
+	def forward(self, x,device):
+		out = self.fc1(x.to(device))
+		out = self.activate(out)
+		out = self.fc2(out)
+		out = self.activate(out)
+		out = self.fc3(out)
+		return out 
